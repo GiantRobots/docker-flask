33629f8d055466b488817f9430549601474c2a01
GiantRobots <alex.scott.thomsen@gmail.com>
2015-08-24 14:55:30 -0700

    Updated to redirect when a route is hit and add a cookie

--- app/app.py
+++ app/app.py
@@ -1,10 +1,19 @@
-from flask import Flask
-
+from flask import Flask, redirect, make_response, request
+import random
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Flask is running!'
+    redirect_to_index = redirect('/lolcats/')
+    response = make_response( redirect_to_index )
+    response.set_cookie('zone', value=str(random.randint(1, 2)))
+    return response
+
+
+@app.route('/lolcats/')
+def lolcats():
+    return 'ZOMG IM THE WORST!' + request.cookies.get('zone', 0)
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=True)
