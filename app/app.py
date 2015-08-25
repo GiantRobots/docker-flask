ec1d903711f72290db89f9b31ef9220e87d37b9f
GiantRobots <alex.scott.thomsen@gmail.com>
2015-08-24 17:51:20 -0700

    Made the flask app reverse proxy shit

--- app/app.py
+++ app/app.py
@@ -1,19 +1,34 @@
 from flask import Flask, redirect, make_response, request
 import random
+import urllib
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    redirect_to_index = redirect('/lolcats/')
-    response = make_response( redirect_to_index )
-    response.set_cookie('zone', value=str(random.randint(1, 2)))
+base_url = "http://todaysinfo.net/"
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def index(path):
+
+    zone_cookie = request.cookies.get('zone', 0)
+    url_fetch = request.url.replace(request.url_root, base_url)
+
+    if zone_cookie != 0:
+        return urllib.urlopen(url_fetch).read()
+    else:
+        redirect_to_index = redirect('/')
+        response = make_response(redirect_to_index)
+        response.set_cookie('zone', value=str(random.randint(1, 2)))
+        return response
+
+
+@app.route('/killcookie/')
+def kill_cookie():
+    response = make_response();
+    response.set_cookie('zone', '', expires=0)
     return response
-
-
-@app.route('/lolcats/')
-def lolcats():
-    return 'Zone Cookie = ' + request.cookies.get('zone', 0)
 
 
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=True)
+
+#/top-15-reasons-hillary-clinton-is-the-worst-mistake-for-president-even-for-liberals/
